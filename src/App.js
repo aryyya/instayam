456c2df71caad4c20cd7aaf962fbeb2eea1ef552
aryyya <aryyya.xyz@gmail.com>
2019-05-07 16:37:21 -0700

    Add basic views.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import './App.css'
 
+// CSS
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bulma'
 
+// Router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Pages
+import NotFoundPage from './pages/not-found-page/not-found-page'
+import LandingPage from './pages/landing-page/landing-page'
 
 const App = () => {
   return (
     <div className="app">
-      instayam
+      <Router>
+        <Switch>
+          {/* Landing Page */}
+          <Route
+            exact
+            path="/"
+            component={LandingPage}
+          />
+          {/* Not Found Page */}
+          <Route
+            path="/"
+            component={NotFoundPage}
+          />
+        </Switch>
+      </Router>
     </div>
   )
 }
