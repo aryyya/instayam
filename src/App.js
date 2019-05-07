70c4e76defcfbe8b1f96345041821857dde3a256
aryyya <aryyya.xyz@gmail.com>
2019-05-07 16:30:30 -0700

    Add necessary packages.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './App.css'
 
-function App() {
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'bulma'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const App = () => {
   return (
     <div className="app">
       instayam
