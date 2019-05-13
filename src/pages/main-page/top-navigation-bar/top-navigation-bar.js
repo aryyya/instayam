0eba3a80adec58ea18337b4997e21721f5585b59
aryyya <aryyya.xyz@gmail.com>
2019-05-13 14:20:55 -0700

    Add various navbar items.

--- src/pages/main-page/top-navigation-bar/top-navigation-bar.js
+++ src/pages/main-page/top-navigation-bar/top-navigation-bar.js
@@ -2,14 +2,43 @@ import React from 'react'
 import './top-navigation-bar.scss'
 
 import InstayamTitle from '../../../common/instayam-title/instayam-title'
+import SearchBar from './search-bar/search-bar'
 
 const TopNavigationBar = () => {
   return (
-    <nav className="top-navigation-bar navbar">
+    <nav className="top-navigation-bar navbar is-fixed-top">
       <div className="container">
         <div className="navbar-brand">
           <div className="navbar-item">
+            <span className="icon is-medium">
+              <i className="fa-lg fab fa-instagram"></i>
+            </span>
+            <div className="top-navigation-bar__logo-separator ml-11 mr-17"></div>
             <InstayamTitle isSmall />
+          </div>
+        </div>
+        <div className="navbar-menu">
+          <div className="top-navigation-bar__center navbar-start">
+            <div className="navbar-item">
+              <SearchBar />
+            </div>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <span className="icon is-medium">
+                <i className="fa-lg far fa-compass"></i>
+              </span>
+            </div>
+            <div className="navbar-item">
+              <span className="icon is-medium">
+                <i className="fa-lg far fa-heart"></i>
+              </span>
+            </div>
+            <div className="navbar-item">
+              <span className="icon is-medium">
+                <i className="fa-lg far fa-user"></i>
+              </span>
+            </div>
           </div>
         </div>
       </div>
