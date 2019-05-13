04a3ef1d58ec3eab5b4ea9a48553c422458f950b
aryyya <aryyya.xyz@gmail.com>
2019-05-13 14:20:36 -0700

    Create dummy search box.

---
+++ src/pages/main-page/top-navigation-bar/search-bar/search-bar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './search-bar.scss'
+
+const SearchBar = () => {
+  return (
+    <input
+      className="search-bar input has-background-white-bis is-shadowless br-2"
+      placeholder="Search"
+    />
+  )
+}
+
+export default SearchBar
